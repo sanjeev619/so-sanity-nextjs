2073816e380e168dba15a6fad83ad41a70c48fc2
sankrit151 <sanjeev@scenarioapp.net>
2024-11-15 23:08:33 +0530

    disable ts temo

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -1,10 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,  // Disable React's Strict Mode
+  reactStrictMode: false, // Disable React's Strict Mode
   env: {
     SC_DISABLE_SPEEDY: "false",
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Disable ESLint errors during production builds
+    ignoreDuringBuilds: true,
+    
+
+    // Custom ESLint configuration
+    // rules: {
+    //   // Add your rules here
+    //   "no-console": "off", // Disables the 'no-console' rule globally
+    //   "react/no-unescaped-entities": "off", // Example rule to disable
+    // },
+    
+  },
 };
 
+module.exports = nextConfig;
+
 export default nextConfig;
+
